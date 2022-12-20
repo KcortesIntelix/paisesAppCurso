fb7b2fe25a689bacb08cf934c13e229b5754c0bf
kcortes <kcortes@intelix.biz>
2022-12-20 17:06:42 -0400

    adelanto

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
+  styles: [`.small-flag{
+    width: 50px;
+}`]
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+
+  termino: string = '';
+  isError: boolean= false;
+  paises : Country[]=[];
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    this.isError = false;
+    console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe( resp =>{
+      console.log(resp)
+      this.paises = resp;
+    },
+    err =>{
+      this.isError=true;
+      this.paises =[];
+    });
   }
 
 }
